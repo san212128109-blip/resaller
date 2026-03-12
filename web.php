284c410534e0c42f99ef002af06a9433d3c92d1a
MN Daily products <san212128109@gmail.com>
2026-03-12 10:49:22 +0600

    Update and rename web.php to vendor.html

--- web.php
+++
@@ -1,9 +0,0 @@
-use App\Http\Controllers\ProductController;
-
-Route::middleware(['auth'])->group(function () {
-    // Reseller Routes
-    Route::post('/reseller/add-product', [ProductController::class, 'store'])->name('product.store');
-
-    // Admin Routes
-    Route::get('/admin/all-products', [ProductController::class, 'adminIndex'])->name('admin.products');
-});
